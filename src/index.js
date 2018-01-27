4d763df8cdc31cf8d9f88b720e6aa2c3671d11d0
Jaylan Jones <jaylanjones421@gmail.com>
2018-01-27 13:50:13 -0600

    completed project and challenges

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store";
+import { HashRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById( "root" )
+);
 registerServiceWorker();
-
